ce44e80c9c7b80c3af8473adef2064742df7dd03
Aaron Steinfeld <aaron@traceable.ai>
2020-08-13 12:34:06 -0700

    feat: make docker java app configurable

--- hypertrace-gradle-docker-java-application-plugin/README.md
+++ hypertrace-gradle-docker-java-application-plugin/README.md
@@ -5,11 +5,19 @@
 ### Purpose
 This plugin is a highly opinionated plugin that configures the target project to build docker images of the target project.
 It configures the default image from `org.hypertrace.docker-plugin` to use the docker file produced by
-`com.bmuschko.docker-java-application`. It allows configuring one value, the base image
-(which defaults to `gcr.io/distroless/java:11-debug`). Additionally, the main class is taken from the application plugin - the
+`com.bmuschko.docker-java-application`. It allows configuring several values, detailed below. 
+Additionally, the main class is taken from the application plugin - the
 default behavior of `com.bmuschko.docker-java-application` for identifying main classes is not used.
-
 An application can define multiple variants which allow specifying different base images to use.
+
+- `baseImage` - String, Defaults to `hypertrace/java:11`
+- `maintainer` - String, Defaults to `Hypertrace 'https://www.hypertrace.org/'`
+- `port` - Integer, no default. Only exposed if set.
+- `adminPort` - Integer, defaults to `${port} + 1` if `port` is set. Only exposed if set.
+- `serviceName` - String, defaults to `${project.name}`. Added as `SERVICE_NAME` env var
+- `healthCheck` - String, defaults to `-interval=2s --start-period=15s --timeout=2s CMD wget  -qO http://127.0.0.1:${adminPort}/health &> /dev/null || exit` if `adminPort` is set. Only added if set.
+- `envVars` - Map<String, String>, defaults to `{"SERVICE_NAME": "${serviceName}"}`
+
 
 For further configuration, use `org.hypertrace.docker-plugin` directly and create a dockerfile.
 
@@ -19,14 +27,36 @@ For further configuration, use `org.hypertrace.docker-plugin` directly and creat
 plugins {
   id("org.hypertrace.docker-java-application-plugin") version "<version>"
 }
+// project.name == "example-app"
 
 application {
   mainClassName = "org.hypertrace.example.app.MyApplication"
 }
 
 hypertraceDocker {
-  javaApplication {
-    baseImage.set("openjdk:11-jre-slim")
+  defaultImage {
+    javaApplication {
+      maintainer.set("Hypertrace 'https://www.hypertrace.org/'")
+      port.set(9001)
+      adminPort.set(9002)
+      serviceName.set("hypertrace-federated-service")
+      healthCheck.set("-interval=2s --start-period=15s --timeout=2s CMD wget -qO- http://127.0.0.1:${adminPort.get()}/health &> /dev/null || exit 1")
+      envVars.put("ENV_VAR", "val")
+    }
   }
 }
+```
+
+Producing a dockerfile:
+```Dockerfile
+FROM hypertrace/java:11
+LABEL maintainer="Hypertrace 'https://www.hypertrace.org/'"
+WORKDIR /app
+COPY libs libs/
+COPY resources resources/
+COPY classes classes/
+ENTRYPOINT ["java", "-cp", "/app/resources:/app/classes:/app/libs/*", "org.hypertrace.example.app.MyApplication"]
+EXPOSE 9001 9002
+HEALTHCHECK -interval=2s --start-period=15s --timeout=2s CMD wget -qO- http://127.0.0.1:9002/health &> /dev/null || exit 1
+ENV SERVICE_NAME=example-app ENV_VAR=val
 ```
--- hypertrace-gradle-docker-java-application-plugin/src/main/java/org/hypertrace/gradle/docker/HypertraceDockerJavaApplication.java
+++ hypertrace-gradle-docker-java-application-plugin/src/main/java/org/hypertrace/gradle/docker/HypertraceDockerJavaApplication.java
@@ -1,24 +1,49 @@
 package org.hypertrace.gradle.docker;
 
 import com.bmuschko.gradle.docker.DockerJavaApplication;
+import com.bmuschko.gradle.docker.tasks.image.Dockerfile;
+import java.util.Collections;
 import javax.inject.Inject;
 import org.gradle.api.NamedDomainObjectContainer;
 import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.plugins.JavaApplication;
+import org.gradle.api.provider.MapProperty;
 import org.gradle.api.provider.Property;
 import org.gradle.api.provider.ProviderFactory;
+import org.gradle.api.tasks.TaskProvider;
 
 public class HypertraceDockerJavaApplication {
 
   public final Property<String> baseImage;
+  public final Property<String> maintainer;
+  public final Property<String> serviceName;
+  public final Property<Integer> port;
+  public final Property<Integer> adminPort;
+  public final Property<String> healthCheck;
+  public final MapProperty<String, String> envVars;
   public final NamedDomainObjectContainer<DockerImageVariant> variants;
   private final Property<String> mainClassName;
 
   @Inject
   public HypertraceDockerJavaApplication(
-      ObjectFactory objectFactory, ProviderFactory providerFactory, JavaApplication application) {
+      ObjectFactory objectFactory, ProviderFactory providerFactory, JavaApplication application, String projectName) {
     this.baseImage = objectFactory.property(String.class)
                                   .convention("hypertrace/java:11");
+    this.maintainer = objectFactory.property(String.class)
+                                   .convention("Hypertrace 'https://www.hypertrace.org/'");
+    this.serviceName = objectFactory.property(String.class)
+                                    .convention(projectName);
+    this.port = objectFactory.property(Integer.class);
+    this.adminPort = objectFactory.property(Integer.class)
+                                  .convention(this.port.map(port -> port + 1));
+    this.envVars = objectFactory.mapProperty(String.class, String.class)
+                                .value(
+                                    this.serviceName.map(serviceName -> Collections.singletonMap("SERVICE_NAME", serviceName))
+                                );
+
+    this.healthCheck = objectFactory.property(String.class)
+                                    .convention(this.adminPort.map(adminPort -> String.format(
+                                        "HEALTHCHECK -interval=2s --start-period=15s --timeout=2s CMD wget -qO- http://127.0.0.1:%d/health &> /dev/null || exit 1", adminPort)));
     this.variants = objectFactory.domainObjectContainer(DockerImageVariant.class,
         name -> objectFactory.newInstance(DockerImageVariant.class, name, this));
     this.mainClassName =
@@ -27,10 +52,21 @@ public class HypertraceDockerJavaApplication {
             .convention(providerFactory.provider(application::getMainClassName));
   }
 
-  void configureDockerJavaApplication(DockerJavaApplication dockerJavaApplication) {
+  void configureDockerJavaApplication(DockerJavaApplication dockerJavaApplication, TaskProvider<Dockerfile> dockerfileTaskProvider) {
     dockerJavaApplication.getBaseImage()
                          .set(this.baseImage);
     dockerJavaApplication.getMainClassName()
                          .set(this.mainClassName);
+    dockerJavaApplication.getMaintainer()
+                         .set(this.maintainer);
+    dockerJavaApplication.getPorts()
+                         .empty()
+                         .add(this.port);
+    dockerJavaApplication.getPorts()
+                         .add(this.adminPort);
+    dockerfileTaskProvider.configure(dockerfile -> {
+      dockerfile.instruction(this.healthCheck);
+      dockerfile.environmentVariable(this.envVars);
+    });
   }
 }
