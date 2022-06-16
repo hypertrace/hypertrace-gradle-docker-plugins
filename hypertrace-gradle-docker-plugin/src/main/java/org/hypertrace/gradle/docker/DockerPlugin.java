22a73fda220b1069953b6f6456f5742e73ee5a90
ravisingal <62086374+ravisingal@users.noreply.github.com>
2022-06-16 22:02:21 +0530

    add task to print docker image default tag (#28)

--- hypertrace-gradle-docker-plugin/src/main/java/org/hypertrace/gradle/docker/DockerPlugin.java
+++ hypertrace-gradle-docker-plugin/src/main/java/org/hypertrace/gradle/docker/DockerPlugin.java
@@ -16,6 +16,7 @@ import org.gradle.api.tasks.TaskProvider;
 public class DockerPlugin implements Plugin<Project> {
   public static final String BUILD_IMAGE_LIFECYCLE_TASK_NAME = "dockerBuildImages";
   public static final String PRINT_DOCKER_IMAGE_WITH_TAGS = "printDockerImageWithTags";
+  public static final String PRINT_DOCKER_IMAGE_DEFAULT_TAG = "printDockerImageDefaultTag";
   public static final String EXTENSION_NAME = "hypertraceDocker";
   public static final String TASK_GROUP = "docker";
   static final String COMMIT_SHA_BUILD_ARG = "COMMIT_SHA";
@@ -28,6 +29,7 @@ public class DockerPlugin implements Plugin<Project> {
     this.configureDockerRegistryCredentials(extension);
     this.addDockerBuildTasks(target, extension);
     this.addPrintDockerImageWithTagsTask(target, extension);
+    this.addPrintDockerImageDefaultTagTask(target);
     this.addDefaultTags(target, extension);
     this.setDefaultImage(target, extension);
   }
@@ -94,11 +96,9 @@ public class DockerPlugin implements Plugin<Project> {
 
   private void addDefaultTags(Project project, DockerPluginExtension extension) {
     if (this.isReleaseVersion(project)) {
-      extension.tag(this.getVersionString(project));
       extension.tag(DockerTag.LATEST, tag -> tag.onlyIf(unused -> extension.tagLatest.get()));
-    } else {
-      extension.tag(this.getBranchTag());
     }
+    extension.tag(this.getDefaultVersionTag(project));
   }
 
   private String getVersionString(Project project) {
@@ -122,6 +122,10 @@ public class DockerPlugin implements Plugin<Project> {
           .map(branch -> branch.replaceAll("[^A-Za-z0-9\\.\\_\\-]", ""))
           .filter(branch -> !branch.isEmpty())
           .orElse("test");
+  }
+
+  private String getDefaultVersionTag(Project project) {
+    return this.isReleaseVersion(project) ? this.getVersionString(project) : this.getBranchTag();
   }
 
   private Optional<String> tryGetBuildSha() {
@@ -180,5 +184,19 @@ public class DockerPlugin implements Plugin<Project> {
             });
           });
         });
+  }
+
+  private void addPrintDockerImageDefaultTagTask(Project project) {
+    project
+      .getTasks()
+      .register(
+        PRINT_DOCKER_IMAGE_DEFAULT_TAG,
+        createdTask -> {
+          createdTask.setDescription("Outputs the default tag of docker images");
+          createdTask.doLast(unused -> {
+            project.getLogger().quiet(getDefaultVersionTag(project));
+          });
+        }
+      );
   }
 }
