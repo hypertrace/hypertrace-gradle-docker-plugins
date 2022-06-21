6a68f6f52eb37460a35933160b1b7ae07deeb59c
Aaron Steinfeld <aaron@traceable.ai>
2022-06-21 12:42:00 -0400

    chore: constrain dependencies

--- hypertrace-gradle-docker-plugin/build.gradle.kts
+++ hypertrace-gradle-docker-plugin/build.gradle.kts
@@ -19,4 +19,11 @@ gradlePlugin {
 
 dependencies {
   api("com.bmuschko:gradle-docker-plugin:7.4.0")
+
+  constraints {
+    api("com.google.guava:guava:30.0-jre")
+    api("com.fasterxml.jackson.core:jackson-databind:2.12.6.1")
+    api("org.bouncycastle:bcprov-jdk15on:1.69")
+    api("commons-io:commons-io:2.7")
+  }
 }
