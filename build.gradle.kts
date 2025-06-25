b355bee1034baf783694f2e05ff8b437cdc391a1
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2025-06-25 18:17:48 -0400

    build: update to use new repositories (#41)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ import org.hypertrace.gradle.publishing.HypertracePublishExtension
 import org.hypertrace.gradle.publishing.License
 
 plugins {
-  id("org.hypertrace.ci-utils-plugin") version "0.3.0"
-  id("org.hypertrace.publish-plugin") version "1.0.4" apply false
-  id("org.hypertrace.repository-plugin") version "0.4.0"
+  id("org.hypertrace.ci-utils-plugin") version "0.4.0"
+  id("org.hypertrace.publish-plugin") version "1.1.1" apply false
+  id("org.hypertrace.repository-plugin") version "0.5.0"
 }
 
 subprojects {
